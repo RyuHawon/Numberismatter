1e42024e6477315d87283c6e6cef48b1bafe4327
RyuHawon <hfixjsj49@gmail.com>
2026-06-11 13:28:20 +0900

    모델즈 작성, 세팅즈 설정, 첫 마이그레이션 커밋

--- game/models.py
+++ game/models.py
@@ -8,3 +8,89 @@ class Character(models.Model):
         on_delete=models.CASCADE,
         related_name="character",
     )
+    max_hp = models.IntegerField(default=100)
+    dice_min = models.IntegerField(default=1)
+    dice_max = models.IntegerField(default=6)
+    permanent_gold = models.IntegerField(default=0)
+    gold_bonus = models.IntegerField(default=0)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "characters"
+
+    def __str__(self):
+        return f"{self.user.nickname}'s character"
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    max_level = models.IntegerField(default=1)
+    effect_per_level = models.FloatField()
+
+    class Meta:
+        db_table = 'skills'
+    
+    def __str__(self):
+        return self.name
+
+
+class Upgrade(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    cost = models.IntegerField()
+    effect = models.CharField(max_length=100)
+    max_level = models.IntegerField(default=1)
+    effect_per_level = models.FloatField()
+
+    class Meta:
+        db_table = 'upgrades'
+
+    def __str__(self):
+        return self.name
+
+
+class CharacterUpgrade(models.Model):
+    character = models.ForeignKey(
+        Character,
+        on_delete=models.CASCADE,
+        related_name='upgrades',
+    )
+    upgrade = models.ForeignKey(
+        Upgrade,
+        on_delete=models.PROTECT,
+        related_name='character_upgrades',
+    )
+    level = models.IntegerField(default=1)
+
+    class Meta:
+        db_table = 'character_upgrades'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['character', 'upgrade'],
+                name='unique_character_upgrade',
+            )
+        ]
+    
+    def __str__(self):
+        return f"{self.character} - {self.upgrade.name} Lv.{self.level}"
+
+
+class Enemy(models.Model):
+    name = models.CharField(max_length=100)
+    hp = models.IntegerField()
+    dice_min = models.IntegerField()
+    dice_max = models.IntegerField()
+    act = models.IntegerField()
+    is_boss = models.BooleanField(default=False)
+    gold_dice_min = models.IntegerField(default=1)
+    gold_dice_max = models.IntegerField(default=20)
+
+    class Meta:
+        db_table = 'enemies'
+        indexes = [
+            models.Index(fields=['act'], name='idx_enemy_act'),
+        ]
+    
+    def __str__(self):
+        return f"[Act{self.act}] {self.name}"
--- numberismatter/settings.py
+++ numberismatter/settings.py
@@ -31,13 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # local apps
+    'accounts',
+    'game',
 ]
+
+# Custom User
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -115,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Login/Logout redirect
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
