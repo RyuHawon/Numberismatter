456a2f3c35459d08e67e7027fd6de9a83b57f4df
RyuHawon <hfixjsj49@gmail.com>
2026-06-11 13:49:16 +0900

    각 앱 admin.py 작성

--- game/admin.py
+++ game/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Character, Skill, Upgrade, CharacterUpgrade, Enemy
+
+
+admin.site.register(Character)
+admin.site.register(Skill)
+admin.site.register(Upgrade)
+admin.site.register(CharacterUpgrade)
+admin.site.register(Enemy)
