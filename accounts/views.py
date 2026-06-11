0a389e51d92e2c646d15883e5aba44f6ac7885bf
RyuHawon <hfixjsj49@gmail.com>
2026-06-11 15:58:59 +0900

    커스텀 유저모델을 폼에 연결하고 회원가입 뷰, 템플릿 작성

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import SignUpForm
+
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('accounts:login')
