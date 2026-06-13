6711f20cb5ec62ff9ad655479316e631965faae4
RyuHawon <hfixjsj49@gmail.com>
2026-06-13 18:32:45 +0900

    회원정보 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,20 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from .forms import SignUpForm
+from .forms import ProfileEditForm, SignUpForm
 
 
 class SignUpView(CreateView):
     form_class = SignUpForm
     template_name = "accounts/signup.html"
     success_url = reverse_lazy("accounts:login")
+
+
+class ProfileEditView(LoginRequiredMixin, UpdateView):
+    form_class = ProfileEditForm
+    template_name = "accounts/profile_edit.html"
+    success_url = reverse_lazy("game:home")
+
+    def get_object(self):
+        return self.request.user
