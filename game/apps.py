88351486d6b6ea4efa093e1490b1b488d976960f
RyuHawon <hfixjsj49@gmail.com>
2026-06-13 15:35:27 +0900

    유저 가입 시 연결된 캐릭터를 만드는 시그널 작성

--- game/apps.py
+++ game/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class GameConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "game"
+
+    def ready(self):
+        from . import signals  # noqa: F401
