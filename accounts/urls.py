f2dcea6765e991eb01311538b9f09d393bab1870
RyuHawon <hfixjsj49@gmail.com>
2026-06-13 18:37:03 +0900

    urls 오타수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("profile/edit/", views.ProfileEditForm.as_view(), name="profile_edit"),
+    path("profile/edit/", views.ProfileEditView.as_view(), name="profile_edit"),
 ]
